d3d6368b4840bb63b25c81af773066f41bdcb588
Dibyajyoti Sahu <dibyajyotisahu.sakti@gmail.com>
2024-12-07 12:08:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ document.getElementById('contactForm').addEventListener('submit', function (e) {
   e.preventDefault();
 
   const name = document.getElementById('name').value;
+  const phone = document.getElementById('phone').value;
   const email = document.getElementById('email').value;
   const message = document.getElementById('message').value;
 
-  console.log("User Data Submitted:", { name, email, message });
+  console.log("User Data Submitted:", { name, phone, email, message });
 
   const responseMessage = document.getElementById('responseMessage');
   responseMessage.textContent = "Thank you for reaching out. We'll get back to you soon.";
